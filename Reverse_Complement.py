f2cc3a3813318a0edb5788c05460a1156f340909
cwoolfo1 <70346478+cwoolfo1@users.noreply.github.com>
2021-02-23 20:41:56 -0500

    Add files via upload

---
+++ Reverse_Complement.py
@@ -0,0 +1,31 @@
+def reverse_complement(input_seq):
+    string = input_seq
+    
+    res = []
+    res[:] = string
+    res.reverse()
+    # seperates the string into a list of characters and reverse the order of the list
+    
+    for i in range(len(string)):
+        
+        if (res[i] == 'A'):
+            res[i] = 'T'
+            
+        elif (res[i] == 'T'):
+            res[i] = 'A'
+            
+        elif (res[i] == 'C'):
+            res[i] = 'G'
+            
+        elif (res[i] == 'G'):
+            res[i] = 'C'
+    
+    # converts the list of DNA nucleotides to its reverse complement.
+    
+    finalstring = ''.join(res)
+    
+    return finalstring
+
+print("Enter a sequence:")
+input_seq = input()
+print(reverse_complement(input_seq))
